98254bb4015e8efe921315abbc4f427d45639b82
mishkat01 <mishkatislam007@gmail.com>
2025-07-22 23:17:36 +0600

    change data type

--- database/migrations/2025_07_21_162133_create_input_single_columns_table.php
+++ database/migrations/2025_07_21_162133_create_input_single_columns_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('input_single_columns', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->json('data')->nullable(); 
+            $table->text('value');
             $table->timestamps();
         });
     }
