a71b2f2dedbd1713a5e45b66f1529822194eaf47
mishkat01 <mishkatislam007@gmail.com>
2025-07-29 23:34:05 +0600

    auth done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 {{-- resources/views/auth/register.blade.php --}}
-@extends('layouts.app')
+@extends('auth.layout.auth-master')
 
 @section('content')
 <div class="min-h-screen flex items-center justify-center bg-gray-100 px-4">
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('auth.layout.auth-master')
 
 @section('content')
 <div class="container">
